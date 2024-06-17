7c6ceb51f78a59252b7496755ead0fd5a8f76179
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2024-06-17 00:12:51 -0300

    Implements Celsius to Fahrenheit converter

--- temperature_converter.py
+++ temperature_converter.py
@@ -2,8 +2,12 @@ class TemperatureConverter:
     def __init__():
         pass
 
-    def celsius_to_fahrenheit():
-        pass
+    def celsius_to_fahrenheit(celsius_value):
+        return celsius_value * (9/5) + 32
 
     def fahrenheit_to_celsius():
         pass
+
+# Testing Area
+result = TemperatureConverter.celsius_to_fahrenheit(0)
+print('Result:', result)
