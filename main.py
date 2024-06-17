bf330190985d4880c08c4b85812c6b8764dbedc0
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2024-06-17 00:31:33 -0300

    Implements Temperature Converter menu

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from measurement_converter import MeasurementConverter
+from temperature_converter import TemperatureConverter
 
 print('''
 [1]. Centimeters to Meters
 [2]. Meters to Centimeters
+[3]. Celsius to Fahrenheit
+[4]. Fahrenheit to Celsius
 ''')
 
 choice = input('Choose an option: ')
@@ -12,5 +15,9 @@ if choice == '1':
     result = MeasurementConverter.centimeters_to_meters(value)
 elif choice == '2':
     result = MeasurementConverter.meters_to_centimeters(value)
+elif choice == '3':
+    result = TemperatureConverter.celsius_to_fahrenheit(value)
+else:
+    result = TemperatureConverter.fahrenheit_to_celsius(value)
     
 print('Result:', result)
