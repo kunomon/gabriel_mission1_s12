11d53f5335357804603a474e5d496c5f7f14c1f3
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2024-06-16 23:59:48 -0300

    Implements Centimeters to Meters converter

--- measurement_converter.py
+++ measurement_converter.py
@@ -2,8 +2,12 @@ class MeasurementConverter:
     def __init__():
         pass
 
-    def centimeters_to_meters():
-        pass
+    def centimeters_to_meters(value_cm):
+        return value_cm / 100
 
     def meters_to_centimeters():
         pass
+
+# Testing Area
+result = MeasurementConverter.centimeters_to_meters(100)
+print('Result:', result)
