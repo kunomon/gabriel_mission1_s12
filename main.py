4db1be66089f7ee5c427c18039990ed62aa132ca
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2024-06-17 00:27:59 -0300

    Implements Meters to Centimeters menu converter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from measurement_converter import MeasurementConverter
 
 print('''
 [1]. Centimeters to Meters
+[2]. Meters to Centimeters
 ''')
 
 choice = input('Choose an option: ')
@@ -9,5 +10,7 @@ value = float(input('Type a value: '))
 
 if choice == '1':
     result = MeasurementConverter.centimeters_to_meters(value)
+elif choice == '2':
+    result = MeasurementConverter.meters_to_centimeters(value)
     
 print('Result:', result)
